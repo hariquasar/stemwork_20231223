d2396e3f282ae2fb78cb5b0604a495673c0c4757
hariquasar <138948301+hariquasar@users.noreply.github.com>
2023-12-23 10:59:50 +0800

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,19 @@
+import tkinter as tk
+from tkinter import messagebox
 
+window = tk.Tk()
+window.title("Tic Tac Toe")
+
+def create_board():
+    for i in range(3):
+        for j in range(3):
+            button = tk.Button(window, text="", font=("Comic Sans MS",50),
+                               height=2,width=6,bg="lightblue", command=lambda row=i, col=j: handle_click(row,col))
+            button.grid(row=i, column=j, sticky="nsew")
+
+create_board()
+
+def handle_click(row,col):
+    pass
+
+window.mainloop()
