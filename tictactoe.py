5d967a26560284eebb1ce5e1493b3fe525e30fa9
hariquasar <138948301+hariquasar@users.noreply.github.com>
2023-12-23 11:30:38 +0800

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -13,7 +13,49 @@ def create_board():
 
 create_board()
 
+board = [[0,0,0],[0,0,0],[0,0,0]]
+current_player = 1
+
 def handle_click(row,col):
-    pass
+    global current_player
+
+    if board[row][col] == 0:
+        if current_player == 1:
+            board[row][col] = "X"
+            current_player = 2
+        else:
+            board[row][col] = "O"
+            current_player = 1
+
+        button = window.grid_slaves(row = row, column = col)[0]
+        button.config(text=board[row][col])
+
+        check_for_winner()
+
+def check_for_winner():
+    winner = None
+
+    for row in board:
+        if row.count(row[0]) == len(row) and row[0] != 0:
+            winner = row[0]
+            break
+    
+    for col in range(len(board)):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != 0:
+            winner = board[0][col]
+            break
+
+    if all([all(row) for row in board]) and winner is None:
+        winner = "tie"
+
+    if winner:
+        declare_winner(winner)
+
+def declare_winner(winner):
+    if winner == "tie":
+        message = "It is a tie!"
+    else:
+        message = f"Player {winner} wins!"
+    answer = messagebox.showinfo("Game Over", message)
 
 window.mainloop()
